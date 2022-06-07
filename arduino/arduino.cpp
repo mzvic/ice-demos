88a25b2494daac056b97134123990f1b88462412
mzvic <vmontesinos2021@udec.cl>
2022-06-07 19:43:08 -0400

    saving

---
+++ arduino/arduino.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "usage" <<endl;
+    cout << "t: take photo" <<endl;
+    cout << "s: shutdown server" <<endl;
+    cout << "q: quit" <<endl;
+    
+    return 0;
+
+}
