89ec81ed5d9c55b1bc3ac3f81fb9f5c847370802
mzvic <vmontesinos2021@udec.cl>
2022-06-07 20:11:56 -0400

    failing in loops

--- arduino/arduino.cpp
+++ arduino/arduino.cpp
@@ -2,13 +2,85 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+    string command;
+    float number;
+    while (true)
+    {
+        while (command != "a")
+        {
+            cout << "usage:" << endl;
+            cout << "l: move to left" << endl;
+            cout << "r: move to right" << endl;
+            cout << "c: move to center" << endl;
+            cout << "a: activate time" << endl;
+            cout << "x: quit" << endl;
+            cin >> command;
+            if (command == "l")
+            {
+                cout << "move to left" << endl;
+            }
+            else if (command == "r")
+            {
+                cout << "move to right" << endl;
+            }
+            else if (command == "c")
+            {
+                cout << "move to center" << endl;
+            }
 
-    cout << "usage" <<endl;
-    cout << "t: take photo" <<endl;
-    cout << "s: shutdown server" <<endl;
-    cout << "q: quit" <<endl;
-    
+            else if (command == "x")
+            {
+                cout << "quit" << endl;
+                break;
+            }
+            else if (command == "a")
+            {
+                ;
+            }
+            else
+            {
+                cout << "invalid command" << endl;
+            }
+        }
+
+        cout << "input time (float): " << endl;
+        cin >> number;
+        cout << "usage:" << endl;
+        cout << "l: move to left" << endl;
+        cout << "r: move to right" << endl;
+        cout << "c: move to center" << endl;
+        cout << "a: deactive time" << endl;
+        cout << "x: quit" << endl;
+        cin >> command;
+        if (command == "l")
+        {
+            cout << "move to left with " << number << "ms." << endl;
+        }
+        else if (command == "r")
+        {
+            cout << "move to right" << number << "ms." << endl;
+        }
+        else if (command == "c")
+        {
+            cout << "move to center" << number << "ms." << endl;
+        }
+
+        else if (command == "x")
+        {
+            cout << "quit" << endl;
+            break;
+        }
+        else if (command == "a")
+        {
+            ;
+        }
+        else
+        {
+            cout << "invalid command" << endl;
+        }
+    }
+
     return 0;
-
 }
